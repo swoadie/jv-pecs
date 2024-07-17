3b8f3eb1ccd57135d600e2359a8f118f031bfaee
Margaryta <marharytayarmoliuk@gmail.com>
2024-07-17 20:40:41 +0300

    Fixed all problems

--- src/main/java/core/mate/academy/service/MachineProducer.java
+++ src/main/java/core/mate/academy/service/MachineProducer.java
@@ -1,8 +1,9 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Machine;
 import java.util.List;
 
-public interface MachineProducer<T> {
+public interface MachineProducer<T extends Machine> {
 
     List<T> get();
 }
